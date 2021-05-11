5d0e6d0a42184de7cef375c42ba02afd341d45cf
phanvigiaii_1430 <phanvigiaii@gmail.com>
2021-05-11 10:56:26 +0700

    added new event

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,10 +3,80 @@ class TodoApp {
         this.inputField = inputField;
         this.todoList = todoList;
     }
+
+    addLocalStorage(item) {
+        if (!localStorage.getItem("list-item")) {
+            const listItem = [];
+            listItem.push(item);
+            localStorage.setItem("list-item", JSON.stringify(listItem));
+        } else {
+            const listItem = JSON.parse(localStorage.getItem("list-item"));
+            listItem.push(item);
+            localStorage.setItem("list-item", JSON.stringify(listItem));
+        }
+    }
+
     addNewTodo(value) {
-        let li = document.createElement("li");
+        const li = document.createElement("li");
         li.innerText = value;
+        const tag1 = this.createTaga(
+            "item-done",
+            '<i class="fas fa-check"></i>'
+        );
+        const tag2 = this.createTaga(
+            "item-delete",
+            '<i class="fas fa-trash"></i>'
+        );
+        const div = this.createDiv(tag1, tag2);
+        li.appendChild(div);
         this.todoList.appendChild(li);
+        this.addLocalStorage(li.innerHTML);
+    }
+
+    createDiv(...a) {
+        const div = document.createElement("div");
+        a.forEach((item) => {
+            div.appendChild(item);
+        });
+        return div;
+    }
+
+    findOuterParent(child) {
+        if (!child) return;
+
+        while (true) {
+            if (child != null && child.tagName === "LI") break;
+            child = child.parentElement;
+        }
+
+        return child;
+    }
+
+    checkDoneTodoItem(parent) {
+        parent.className = "checked";
+        parent.style.textDecoration = "line-through";
+    }
+
+    removeTodoItem(parent) {
+        parent.remove();
+    }
+
+    createTaga(className, child) {
+        const a = document.createElement("a");
+        a.className = className;
+        a.innerHTML = child;
+        if (className === "item-done") {
+            a.addEventListener("click", () => {
+                const outerParent = this.findOuterParent(a);
+                this.checkDoneTodoItem(outerParent);
+            });
+        } else {
+            a.addEventListener("click", () => {
+                const outerParent = this.findOuterParent(a);
+                this.removeTodoItem(outerParent);
+            });
+        }
+        return a;
     }
 }
 
@@ -29,3 +99,5 @@ inputField.addEventListener("keydown", function (key) {
         this.value = "";
     }
 });
+
+console.log(localStorage.getItem("item"));
